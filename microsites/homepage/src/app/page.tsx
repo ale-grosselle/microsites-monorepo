eba56039aeca7624760b23caab43a2945b3f99f8
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-06-23 16:39:38 +0200

    chore: some little fix

--- microsites/homepage/src/app/page.tsx
+++ microsites/homepage/src/app/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from "@packages/ui/acmeLogo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
 import Image from "next/image";
 
 export default function Page() {
