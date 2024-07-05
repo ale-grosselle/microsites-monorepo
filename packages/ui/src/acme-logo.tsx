bb0461292750d6762ea10f3aff6e2058f1c47008
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-07-05 17:15:37 +0200

    feat: Acme

--- packages/ui/src/acme-logo.tsx
+++ packages/ui/src/acme-logo.tsx
@@ -1,13 +1,10 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`flex flex-row items-center leading-none text-white`}
-    >
+    <div className={`flex flex-row items-center leading-none text-white`}>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <p className="text-[44px]">Acme Text</p>
     </div>
   );
 }
