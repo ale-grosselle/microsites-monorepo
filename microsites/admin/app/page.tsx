74173a23b874ce72a81c98971f938d0aa923c7d8
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-07-05 17:32:52 +0200

    change

--- microsites/admin/app/page.tsx
+++ microsites/admin/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
-      <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>The Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card />
         <Card />
