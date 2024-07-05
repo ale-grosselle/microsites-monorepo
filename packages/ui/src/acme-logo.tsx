b267e61cc10af973da65cb9c0acb625ae3f2f7c2
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-07-05 17:22:26 +0200

    chore: acme

--- packages/ui/src/acme-logo.tsx
+++ packages/ui/src/acme-logo.tsx
@@ -4,7 +4,7 @@ export default function AcmeLogo() {
   return (
     <div className={`flex flex-row items-center leading-none text-white`}>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme Text</p>
+      <p className="text-[44px]">Acme Text!</p>
     </div>
   );
 }
