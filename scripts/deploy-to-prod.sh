2f93047c4aca30f1991e6766b1da756c52b44ba9
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-07-08 15:39:24 +0200

    feat: update

--- scripts/deploy-to-prod.sh
+++ scripts/deploy-to-prod.sh
@@ -8,7 +8,12 @@ PACKAGE_NAME=$1
 
 echo "Deploying package $PACKAGE_NAME to production..."
 
-echo "Just for instance we are using fly.io to deploy the package..."
+echo "install fly.io..."
+curl -L https://fly.io/install.sh | sh
+export FLYCTL_INSTALL="/home/runner/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
+
+
 FLY_TOML="microsites/${PACKAGE_NAME#'@micro-site/'}/fly.toml"
 if [ ! -f "$FLY_TOML" ]; then
   echo "Error: Fly configuration file $FLY_TOML not found."
