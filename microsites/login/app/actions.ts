1c4a32932789c731d9ce3958c9c9b52c2bb98400
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-07-12 11:01:02 +0200

    feat: add a console log

--- microsites/login/app/actions.ts
+++ microsites/login/app/actions.ts
@@ -2,6 +2,7 @@
 //import { redirect } from "next/navigation";
 
 export async function submitLogin() {
+  console.log("HELLO LOGIN");
   //redirect(`https://localhost:8443/dashboard`);
   return true;
 }
